5492ce50ec27155019e5592774e32d8a56fd0cc0
tushar17s <tusharsaini1703@gmail.com>
2026-02-06 17:52:46 +0530

    b

--- vault_core/views.py
+++ vault_core/views.py
@@ -13,8 +13,8 @@ from drf_spectacular.utils import extend_schema
 
 # Create your views here.
 @extend_schema(
-    request=ResourceSerializers,
-    responses=ResourceSerializers
+    request=RegisterSerializers,
+    responses=RegisterSerializers
 )
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticated]) # adds authentication layer for validating user
